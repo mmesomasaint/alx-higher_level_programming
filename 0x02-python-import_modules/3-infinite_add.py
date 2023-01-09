f5bd80a9e16d5f1b938041cbb4732224fe84cf4c
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-01-09 17:56:30 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    
     result = 0
     count = len(sys.argv)
-    if count > 0:
-        for i in range(count):
+    if count == 1: 
+        result = sys.argv[i]
+    
+    elif count > 1:
+        for i in range(1, count-1):
             result += int(sys.argv[i])
     print("{:d}".format(result))
