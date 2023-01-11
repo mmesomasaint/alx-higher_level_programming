d39030555135dc35b36271dddf3f8a7382caf281
mmesomasaint <mmesomasaint@gmail.com>
2023-01-11 07:26:38 -0800

    Updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    
     count -= 1
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
