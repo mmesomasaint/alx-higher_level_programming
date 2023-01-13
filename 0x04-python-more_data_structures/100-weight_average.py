d68b09d281e07c6ff1b78e56ae8c6100341ef9aa
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-01-13 16:55:53 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,15 +1,12 @@
-
 #!/usr/bin/python3
-
-
 def weight_average(my_list=[]):
-    """Return the weighted average of all integers in a list of tuples."""
-    if not isinstance(my_list, list) or len(my_list) == 0:
+    if my_list is not None:
+        total = 0
+        frequency = 0
+        for tup in my_list:
+            (weight, occurence) = tup
+            total += (weight * occurence)
+            frequency += occurence
+        return (total/frequency) if frequency > 0 else 0
+    else:
         return (0)
-
-    avg = 0
-    size = 0
-    for tup in my_list:
-        avg += (tup[0] * tup[1])
-        size += tup[1]
-    return (avg / size)
