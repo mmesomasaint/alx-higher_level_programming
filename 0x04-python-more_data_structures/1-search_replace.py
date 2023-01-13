d32f10fd6823d043c9f674d9924472c5d476bac2
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-01-13 16:22:09 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
-    idx = []
-    for i, value in enumerate(my_list):
-        if value == search:
-            idx.append(i)
-    for index in idx:
-        my_list[index] = replace
-    return my_list
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
